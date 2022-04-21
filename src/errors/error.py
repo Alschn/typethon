80b1310b7379ab1b74b33df0d467ee8da57cec19
Alschn <adam.lisichin@gmail.com>
2022-04-21 20:35:47 +0200

    Add position to LexerError

--- src/errors/error.py
+++ src/errors/error.py
@@ -1,11 +1,19 @@
+from src.lexer.position import Position
+
+
 class Error(Exception):
     def __init__(self, message: str):
         super().__init__(message)
 
 
 class LexerError(Error):
-    def __init__(self, message: str):
+    def __init__(self, message: str, position: Position):
         super().__init__(message)
+        self.message = message
+        self.position = position
+
+    def __str__(self) -> str:
+        return f"{self.message} Line: {self.position.line} Column: {self.position.column}"
 
 
 class ParserError(Error):
