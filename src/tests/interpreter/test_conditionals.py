b6a8424863a49718e18d7e999b520c88122ffba3
Alschn <adam.lisichin@gmail.com>
2022-06-06 17:18:58 +0200

    Add conditionals tests

--- src/tests/interpreter/test_conditionals.py
+++ src/tests/interpreter/test_conditionals.py
@@ -6,6 +6,7 @@ from src.errors.interpreter import UnexpectedTypeError
 from src.tests.utils import setup_interpreter, mock_stdout
 
 
+# noinspection PyMethodMayBeStatic
 class InterpreterConditionalsTests(unittest.TestCase):
 
     @mock_stdout
@@ -28,60 +29,76 @@ class InterpreterConditionalsTests(unittest.TestCase):
         setup_interpreter(text).interpret()
         self.assertEqual(stdout.getvalue(), "")
 
-    def test_if_statement_true_condition_with_elifs(self):
+    @mock_stdout
+    def test_if_statement_true_condition_with_elifs(self, stdout):
         text = """
-        if () {
-        
-        } elif () {
-        
-        } elif () {
-        
+        if (true or true or true or false) {
+            const a: int = 0;
+            print(a, "<- inside if");
+        } elif (true) {
+            print("this won't run");
+        } elif (null) {
+            print("this won't run as well");
         }
         """
+        setup_interpreter(text).interpret()
+        self.assertEqual(stdout.getvalue().strip(), "0 <- inside if")
 
     def test_if_statement_false_condition_one_of_elifs_true(self):
         text = """
-        if () {
-        
-        } elif () {
-        
-        } elif () {
-        
-        } else () {
-        
-        }
+        if (false) {
+            const a: int = 11;
+        } elif (null) {
+            const a: int = 13;
+        } elif (true) {
+            const a: int = 15;
+        } else { while(true) {} }
         """
+        interpreter = setup_interpreter(text)
+        interpreter.interpret()
+        a = interpreter.env.get_variable('a')
+        self.assertIsNone(a)  # variable exists in local scope, not in global scope
 
-    def test_if_statement_false_condition_all_elif_false_condition(self):
+    @mock_stdout
+    def test_if_statement_false_condition_all_elif_false_condition(self, stdout):
         text = """
-        if () {
-
-        } elif () {
-
-        } elif () {
-
-        } else () {
-
+        if (null) {
+            while (true) {}
+        } elif (false or not true) {
+            while (true) {
+                while (true) {}
+            } 
+        } elif (false or false) {
+            while (true) {}
+        } else {
+            print("this will run");
         }
         """
+        setup_interpreter(text).interpret()
+        self.assertEqual(stdout.getvalue(), "this will run\n")
 
-    def test_if_statement_true_condition_with_else(self):
+    @mock_stdout
+    def test_if_statement_true_condition_with_else(self, stdout):
         text = """
-        if () {
-
+        if (true) {
+            print("xd");
         } else () {
-
+            while (true) {}
         }
         """
+        setup_interpreter(text).interpret()
+        self.assertEqual(stdout.getvalue(), "xd\n")
 
     def test_if_statement_false_condition_with_else(self):
         text = """
-        if () {
-
-        } else () {
-
+        if (false) {
+            while (true) {}
+        } else {
+            const a: func(() => void) = (): void => {};
+            a();
         }
         """
+        setup_interpreter(text).interpret()
 
     @mock_stdout
     def test_if_statement_false_condition_all_elif_false_condition_else_runs(self, stdout):
@@ -100,27 +117,27 @@ class InterpreterConditionalsTests(unittest.TestCase):
         setup_interpreter(text).interpret()
         self.assertEqual(stdout.getvalue(), "else\n")
 
-    def test_nested_if_statements(self):
+    @mock_stdout
+    def test_nested_if_statements(self, stdout):
         text = """
         if (true) {
-            if () {
-                if () {
-                
+            print("1");
+            if (true) {
+                print("2");
+                if (false) {
+                    print("3");
                 }
             }
-            elif () {
-            
+            elif (true) {
+                print("4");
             }
         } else {
-            if (false / null * true) {}
+            if (false / null * true) { print(5); }
         }
-        
+        print(7);
         """
-
-    def test_nested_if_statements_elif_else_in_wrong_places(self):
-        text = """
-
-        """
+        setup_interpreter(text).interpret()
+        self.assertEqual(stdout.getvalue(), "1\n2\n7\n")
 
     @mock_stdout
     def test_if_statement_function_call_as_condition(self, stdout):
@@ -130,7 +147,6 @@ class InterpreterConditionalsTests(unittest.TestCase):
         """
         setup_interpreter(text).interpret()
         self.assertEqual(stdout.getvalue(), "condition\n")
-
 
     @parameterized.expand([
         ('1',),
